774fc443b07872d20473c6b6006d076c0b3f48cf
Yahya <tech1@lasega.com>
2020-11-12 16:10:08 +0200

    added route to laptop history for a user

--- private/KAASoft/Environment/Routes/Admin/UserRoutes.php
+++ private/KAASoft/Environment/Routes/Admin/UserRoutes.php
@@ -1,72 +1,98 @@
 <?php
-    /**
-     * Copyright (c) 2015 - 2017 by KAA Soft. All rights reserved.
-     */
 
-    namespace KAASoft\Environment\Routes\Admin;
+/**
+ * Copyright (c) 2015 - 2017 by KAA Soft. All rights reserved.
+ */
 
-    use KAASoft\Environment\Routes\AdminRoute;
-    use KAASoft\Environment\Routes\PublicRoute;
-    use KAASoft\Environment\Routes\RoutesInterface;
+namespace KAASoft\Environment\Routes\Admin;
 
-    /**
-     * Class UserRoutes
-     * @package KAASoft\Environment\Routes\Admin
-     */
-    class UserRoutes implements RoutesInterface {
+use KAASoft\Environment\Routes\AdminRoute;
+use KAASoft\Environment\Routes\PublicRoute;
+use KAASoft\Environment\Routes\RoutesInterface;
 
-        const USER_LIST_VIEW_ROUTE    = "userListView";
-        const USER_CREATE_ROUTE       = "userCreate";
-        const USER_EDIT_ROUTE         = "userEdit";
-        const USER_DELETE_ROUTE       = "userDelete";
-        const USER_EMAIL_CHECK_ROUTE  = "userEmailCheck";
-        const USER_PHOTO_UPLOAD_ROUTE = "userPhotoUpload";
-        const USER_SEARCH_ROUTE       = "userSearch";
+/**
+ * Class UserRoutes
+ * @package KAASoft\Environment\Routes\Admin
+ */
+class UserRoutes implements RoutesInterface
+{
 
-        public static function getRoutes() {
-            $routes = [];
-            /*************************************  USER  **********************************************************/
-            $routes[UserRoutes::USER_LIST_VIEW_ROUTE] = new AdminRoute(_("User List View"),
-                                                                       "/users(?:/page/(\\d+))?[/]??",
-                                                                       "Admin\\User\\UsersAction",
-                                                                       "/users",
-                                                                       [ "page" ]);
+    const USER_LIST_VIEW_ROUTE    = "userListView";
+    const USER_CREATE_ROUTE       = "userCreate";
+    const USER_EDIT_ROUTE         = "userEdit";
+    const USER_Laptop_ROUTE         = "userLaptop";
+    const USER_DELETE_ROUTE       = "userDelete";
+    const USER_EMAIL_CHECK_ROUTE  = "userEmailCheck";
+    const USER_PHOTO_UPLOAD_ROUTE = "userPhotoUpload";
+    const USER_SEARCH_ROUTE       = "userSearch";
 
-            $routes[UserRoutes::USER_CREATE_ROUTE] = new AdminRoute(_("User Create"),
-                                                                    "/user/create[/]??",
-                                                                    "Admin\\User\\UserCreateAction",
-                                                                    "/user/create/");
+    public static function getRoutes()
+    {
+        $routes = [];
+        /*************************************  USER  **********************************************************/
+        $routes[UserRoutes::USER_LIST_VIEW_ROUTE] = new AdminRoute(
+            _("User List View"),
+            "/users(?:/page/(\\d+))?[/]??",
+            "Admin\\User\\UsersAction",
+            "/users",
+            ["page"]
+        );
 
-            $routes[UserRoutes::USER_EDIT_ROUTE] = new AdminRoute(_("User Edit"),
-                                                                  "/user/(\\d+)/edit[/]??",
-                                                                  "Admin\\User\\UserEditAction",
-                                                                  "/user/[userId]/edit",
-                                                                  [ "userId" ]);
+        $routes[UserRoutes::USER_CREATE_ROUTE] = new AdminRoute(
+            _("User Create"),
+            "/user/create[/]??",
+            "Admin\\User\\UserCreateAction",
+            "/user/create/"
+        );
+
+        $routes[UserRoutes::USER_EDIT_ROUTE] = new AdminRoute(
+            _("User Edit"),
+            "/user/(\\d+)/edit[/]??",
+            "Admin\\User\\UserEditAction",
+            "/user/[userId]/edit",
+            ["userId"]
+        );
+
+        $routes[UserRoutes::USER_Laptop_ROUTE] = new AdminRoute(
+            _("User Laptop"),
+            "/user/(\\d+)/userlaptopList[/]??",
+            "Admin\\User\\UserLaptopListAction",
+            "/user/[userId]/userlaptopList",
+            ["userId"]
+        );
 
 
-            $routes[UserRoutes::USER_DELETE_ROUTE] = new AdminRoute(_("User Delete"),
-                                                                    "/user/(\\d+)/delete[/]??",
-                                                                    "Admin\\User\\UserDeleteAction",
-                                                                    "/user/[userId]/delete",
-                                                                    [ "userId" ]);
+        $routes[UserRoutes::USER_DELETE_ROUTE] = new AdminRoute(
+            _("User Delete"),
+            "/user/(\\d+)/delete[/]??",
+            "Admin\\User\\UserDeleteAction",
+            "/user/[userId]/delete",
+            ["userId"]
+        );
 
-            $routes[UserRoutes::USER_EMAIL_CHECK_ROUTE] = new PublicRoute(_("User Email Check"),
-                                                                          "/user/check-email[/]??",
-                                                                          "Admin\\User\\UserCheckEmailAction",
-                                                                          "/user/check-email");
+        $routes[UserRoutes::USER_EMAIL_CHECK_ROUTE] = new PublicRoute(
+            _("User Email Check"),
+            "/user/check-email[/]??",
+            "Admin\\User\\UserCheckEmailAction",
+            "/user/check-email"
+        );
 
-            $routes[UserRoutes::USER_PHOTO_UPLOAD_ROUTE] = new AdminRoute(_("User Photo Upload"),
-                                                                          "/user/photo-upload/??",
-                                                                          "Admin\\Image\\UserPhotoUploadAction",
-                                                                          "/user/photo-upload");
+        $routes[UserRoutes::USER_PHOTO_UPLOAD_ROUTE] = new AdminRoute(
+            _("User Photo Upload"),
+            "/user/photo-upload/??",
+            "Admin\\Image\\UserPhotoUploadAction",
+            "/user/photo-upload"
+        );
 
-            $routes[UserRoutes::USER_SEARCH_ROUTE] = new AdminRoute(_("User Search"),
-                                                                    "/user/search[/]??",
-                                                                    "Admin\\User\\UserSearchAction",
-                                                                    "/user/search");
+        $routes[UserRoutes::USER_SEARCH_ROUTE] = new AdminRoute(
+            _("User Search"),
+            "/user/search[/]??",
+            "Admin\\User\\UserSearchAction",
+            "/user/search"
+        );
 
-            /*************************************  USER END  ******************************************************/
+        /*************************************  USER END  ******************************************************/
 
-            return $routes;
-        }
+        return $routes;
     }
+}
